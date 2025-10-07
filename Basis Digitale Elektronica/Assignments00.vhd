c3868863b5ad6180af2fc3abefb298cd4cfe7675
EliasDeHondt <Elias.dehondt@outlook.com>
2025-10-07 09:55:40 +0200

    Rename entity from Assignments00 to Main for consistency; update architecture declaration

--- Basis Digitale Elektronica/Assignments00.vhd
+++ Basis Digitale Elektronica/Assignments00.vhd
@@ -2,16 +2,15 @@
 -- @see https://eliasdh.com
 -- @since 01/01/2025
 
-
 -------------------- Main --------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity Assignments00 is
+entity Main is
     Port ( A, B, C : in STD_LOGIC; G : out STD_LOGIC);
-end Assignments00;
+end Main;
 
-architecture Behavioral of Assignments00 is
+architecture Behavioral of Main is
 
 begin
     G <= (not(A) and (not(B) or C)) or (not((A and not(B)) or not(C))) or (B and C); -- G <= (not(C) or not(A) or B);
