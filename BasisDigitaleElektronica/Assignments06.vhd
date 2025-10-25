885152dbb089b94339940899179d072ee7928c8c
EliasDeHondt <Elias.dehondt@outlook.com>
2025-10-25 12:24:06 +0200

    Add initial implementation of Main entity in Assignments06.vhd

--- BasisDigitaleElektronica/Assignments06.vhd
+++ BasisDigitaleElektronica/Assignments06.vhd
@@ -1,3 +1,26 @@
 -- @author EliasDH Team
 -- @see https://eliasdh.com
 -- @since 01/01/2025
+
+-------------------- Main --------------------
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity Main is
+    port (
+        Dip: in std_logic_vector(1 downto 0);
+        R: out std_logic_vector(1 downto 0);
+        G: out std_logic_vector(1 downto 0);
+        B: out std_logic_vector(1 downto 0);
+        HSync: out std_logic_vector(1 downto 0);
+        VSync: out std_logic_vector(1 downto 0)
+        );
+end Main;
+
+architecture Behavioral of Main is
+
+begin
+
+end Behavioral;
+-------------------- Main --------------------
