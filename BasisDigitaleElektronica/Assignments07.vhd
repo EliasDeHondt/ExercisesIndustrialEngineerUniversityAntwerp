60cc00cb8ee032c235a1d01ad48b959c961575ad
EliasDeHondt <Elias.dehondt@outlook.com>
2025-11-15 13:23:40 +0100

    feat: Add VGA display support and 7-segment display pin assignments in Assignments07.vhd and Assignments07.xdc; remove obsolete SampleExamQuestion.plantuml

--- BasisDigitaleElektronica/Assignments07.vhd
+++ BasisDigitaleElektronica/Assignments07.vhd
@@ -14,19 +14,114 @@ entity Main is
         VGA_G: out std_logic_vector (3 downto 0);
         VGA_B: out std_logic_vector (3 downto 0);
         HSync: out std_logic;
-        VSync: out std_logic
+        VSync: out std_logic;
+        Cathodes: out std_logic_vector(7 downto 0);
+        Anodes: out std_logic_vector(7 downto 0);
+        Leds: out std_logic_vector(15 downto 0)
     );
 end Main;
 
 architecture Behavioral of Main is
-    constant COLOR_ELIASDH_R: std_logic_vector(7 downto 0)  := x"4F";
-    constant COLOR_ELIASDH_G: std_logic_vector(7 downto 0)  := x"94";
-    constant COLOR_ELIASDH_B: std_logic_vector(7 downto 0)  := x"F0";
+    type icon is array (0 to 7) of std_logic_vector(15 downto 0); -- 8 rows of 16 pixels (type definition)
+    
+    constant UM1 : icon := ( -- UM1 (UFO Mothership 1)
+        x"00F0",  -- 0000000011110000
+        x"03FC",  -- 0000001111111100
+        x"07FE",  -- 0000011111111110
+        x"0E67",  -- 0000111001100111
+        x"07FE",  -- 0000011111111110
+        x"01B8",  -- 0000000110111000
+        x"0376",  -- 0000001101110110
+        x"0000"   -- 0000000000000000
+    );
 
-    signal HCounter : integer := 1;                 -- Horizontal pixel counter
-    signal VCounter : integer := 1;                 -- Vertical line counter
-    signal Clk25MHz : std_logic := '0';             -- Derived 25 MHz clock for VGA timing
-    signal Counter  : integer := 0;                 -- Clock divider counter
+    constant OF1 : icon := ( -- OF1 (Octopus Frame 1)
+        x"0010",  -- 0000000000010000
+        x"007C",  -- 0000000001111100
+        x"01FE",  -- 0000000111111110
+        x"036E",  -- 0000001101101110
+        x"03FF",  -- 0000001111111111
+        x"01ED",  -- 0000000111101101
+        x"00C3",  -- 0000000011000011
+        x"0060"   -- 0000000001100000
+    );
+
+    constant OF2 : icon := ( -- OF2 (Octopus Frame 2)
+        x"0010",  -- 0000000000010000
+        x"007C",  -- 0000000001111100
+        x"01FE",  -- 0000000111111110
+        x"036E",  -- 0000001101101110
+        x"03FF",  -- 0000001111111111
+        x"01ED",  -- 0000000111101101
+        x"00D3",  -- 0000000011010011
+        x"0098"   -- 0000000010011000
+    );
+
+    constant CF1 : icon := ( -- CF1 (Crab Frame 1)
+        x"0208",  -- 0000001000001000
+        x"0208",  -- 0000001000001000
+        x"03F0",  -- 0000001111110000
+        x"07F8",  -- 0000011111111000
+        x"06D8",  -- 0000011011011000
+        x"07F8",  -- 0000011111111000
+        x"0E1C",  -- 0000111000011100
+        x"0738"   -- 0000011100111000
+    );
+
+    constant CF2 : icon := ( -- CF2 (Crab Frame 2)
+        x"0208",  -- 0000001000001000
+        x"0208",  -- 0000001000001000
+        x"03F0",  -- 0000001111110000
+        x"07F8",  -- 0000011111111000
+        x"06D8",  -- 0000011011011000
+        x"07F8",  -- 0000011111111000
+        x"0E1C",  -- 0000111000011100
+        x"0C0C"   -- 0000110000001100
+    );
+
+    constant SF1 : icon := ( -- SF1 (Squid Frame 1)
+        x"0008",  -- 0000000000001000
+        x"001C",  -- 0000000000011100
+        x"003E",  -- 0000000000111110
+        x"006B",  -- 0000000001101011
+        x"007F",  -- 0000000001111111
+        x"00E6",  -- 0000000011100110
+        x"0042",  -- 0000000001000010
+        x"00A4"   -- 0000000010100100
+    );
+
+    constant SF2 : icon := ( -- SF2 (Squid Frame 2)
+        x"0008",  -- 0000000000001000
+        x"001C",  -- 0000000000011100
+        x"003E",  -- 0000000000111110
+        x"006B",  -- 0000000001101011
+        x"007F",  -- 0000000001111111
+        x"00E6",  -- 0000000011100110
+        x"002A",  -- 0000000000101010
+        x"0042"   -- 0000000001000010
+    );
+
+    constant CANNON : icon := ( -- Cannon (simple rectangle base with point/barrel on top, centered)
+        x"0180",  -- 0000000110000000
+        x"0180",  -- 0000000110000000
+        x"0180",  -- 0000000110000000
+        x"07E0",  -- 0000011111100000
+        x"0FF0",  -- 0000111111110000
+        x"1FF8",  -- 0001111111111000
+        x"3FFC",  -- 0011111111111100
+        x"0000"   -- 0000000000000000
+    );
+
+
+
+    constant COLOR_ELIASDH_R: std_logic_vector(7 downto 0) := x"4F";
+    constant COLOR_ELIASDH_G: std_logic_vector(7 downto 0) := x"94";
+    constant COLOR_ELIASDH_B: std_logic_vector(7 downto 0) := x"F0";
+
+    signal HCounter: integer := 1;                 -- Horizontal pixel counter
+    signal VCounter: integer := 1;                 -- Vertical line counter
+    signal Clk25MHz: std_logic := '0';             -- Derived 25 MHz clock for VGA timing
+    signal Counter: integer := 0;                  -- Clock divider counter
 begin
     CLOCK_DIVIDER: process (Clk100MHz) is begin -- Generate 25 MHz clock from 100 MHz input 1/4 frequency
         if rising_edge(Clk100MHz) then
@@ -57,7 +152,7 @@ begin
         end if;
     end process VGA_TIMING;
 
-    VGA_SYNC: process (HCounter, VCounter) is begin
+    VGA_SYNC: process (HCounter, VCounter) is begin     -- Standard 640x480@60Hz sync pulses
         if VCounter > 523 and VCounter <= 525 then      -- VSync pulse
             VSync <= '0';
         else
@@ -68,16 +163,22 @@ begin
         else
             HSync <= '1';
         end if;
-        if HCounter <= 688 and HCounter > 48 and VCounter <= 513 and VCounter > 33 then -- Visible area
-            VGA_R <= COLOR_ELIASDH_R(7 downto 4); -- Red component
-            VGA_G <= COLOR_ELIASDH_G(7 downto 4); -- Green
-            VGA_B <= COLOR_ELIASDH_B(7 downto 4); -- Blue
-        else
-            VGA_R <= "0000"; -- No Red
-            VGA_G <= "0000"; -- No Green
-            VGA_B <= "0000"; -- No Blue
+        if HCounter < 640 and VCounter < 480 then -- Visible area
+            if HCounter = 0 or HCounter = 639 or VCounter = 0 or VCounter = 479 then -- Check for border pixels
+                VGA_R <= "1111";
+                VGA_G <= "0000";
+                VGA_B <= "0000";
+            else
+                VGA_R <= COLOR_ELIASDH_R(7 downto 4);
+                VGA_G <= COLOR_ELIASDH_G(7 downto 4);
+                VGA_B <= COLOR_ELIASDH_B(7 downto 4);
+            end if;
         end if;
     end process VGA_SYNC;
 
+    -- Temporary outputs
+    Cathodes <= (others => '1');
+    Anodes <= (others => '1');
+    Leds <= (others => '0');
 end Behavioral;
 -------------------- Main --------------------
--- BasisDigitaleElektronica/Assignments07.xdc
+++ BasisDigitaleElektronica/Assignments07.xdc
@@ -24,6 +24,45 @@ set_property -dict { PACKAGE_PIN D8 IOSTANDARD LVCMOS33 } [get_ports { VGA_B[3] 
 set_property -dict { PACKAGE_PIN B11 IOSTANDARD LVCMOS33 } [get_ports { HSync }];
 set_property -dict { PACKAGE_PIN B12 IOSTANDARD LVCMOS33 } [get_ports { VSync }];
 
+# 7 Segment Display Cathodes 
+set_property -dict { PACKAGE_PIN T10   IOSTANDARD LVCMOS33 } [get_ports { Cathodes[7] }];
+set_property -dict { PACKAGE_PIN R10   IOSTANDARD LVCMOS33 } [get_ports { Cathodes[6] }];
+set_property -dict { PACKAGE_PIN K16   IOSTANDARD LVCMOS33 } [get_ports { Cathodes[5] }];
+set_property -dict { PACKAGE_PIN K13   IOSTANDARD LVCMOS33 } [get_ports { Cathodes[4] }];
+set_property -dict { PACKAGE_PIN P15   IOSTANDARD LVCMOS33 } [get_ports { Cathodes[3] }];
+set_property -dict { PACKAGE_PIN T11   IOSTANDARD LVCMOS33 } [get_ports { Cathodes[2] }];
+set_property -dict { PACKAGE_PIN L18   IOSTANDARD LVCMOS33 } [get_ports { Cathodes[1] }];
+
+set_property -dict { PACKAGE_PIN H15   IOSTANDARD LVCMOS33 } [get_ports { Cathodes[0] }];
+
+# 7 Segment Display Anodes
+set_property -dict { PACKAGE_PIN J17   IOSTANDARD LVCMOS33 } [get_ports { Anodes[7] }];
+set_property -dict { PACKAGE_PIN J18   IOSTANDARD LVCMOS33 } [get_ports { Anodes[6] }];
+set_property -dict { PACKAGE_PIN T9    IOSTANDARD LVCMOS33 } [get_ports { Anodes[5] }];
+set_property -dict { PACKAGE_PIN J14   IOSTANDARD LVCMOS33 } [get_ports { Anodes[4] }];
+set_property -dict { PACKAGE_PIN P14   IOSTANDARD LVCMOS33 } [get_ports { Anodes[3] }];
+set_property -dict { PACKAGE_PIN T14   IOSTANDARD LVCMOS33 } [get_ports { Anodes[2] }];
+set_property -dict { PACKAGE_PIN K2    IOSTANDARD LVCMOS33 } [get_ports { Anodes[1] }];
+set_property -dict { PACKAGE_PIN U13   IOSTANDARD LVCMOS33 } [get_ports { Anodes[0] }];
+
+# LEDs
+set_property -dict { PACKAGE_PIN H17   IOSTANDARD LVCMOS33 } [get_ports { Leds[0] }];
+set_property -dict { PACKAGE_PIN K15   IOSTANDARD LVCMOS33 } [get_ports { Leds[1] }];
+set_property -dict { PACKAGE_PIN J13   IOSTANDARD LVCMOS33 } [get_ports { Leds[2] }];
+set_property -dict { PACKAGE_PIN N14   IOSTANDARD LVCMOS33 } [get_ports { Leds[3] }];
+set_property -dict { PACKAGE_PIN R18   IOSTANDARD LVCMOS33 } [get_ports { Leds[4] }];
+set_property -dict { PACKAGE_PIN V17   IOSTANDARD LVCMOS33 } [get_ports { Leds[5] }];
+set_property -dict { PACKAGE_PIN U17   IOSTANDARD LVCMOS33 } [get_ports { Leds[6] }];
+set_property -dict { PACKAGE_PIN U16   IOSTANDARD LVCMOS33 } [get_ports { Leds[7] }];
+set_property -dict { PACKAGE_PIN V16   IOSTANDARD LVCMOS33 } [get_ports { Leds[8] }];
+set_property -dict { PACKAGE_PIN T15   IOSTANDARD LVCMOS33 } [get_ports { Leds[9] }];
+set_property -dict { PACKAGE_PIN U14   IOSTANDARD LVCMOS33 } [get_ports { Leds[10] }];
+set_property -dict { PACKAGE_PIN T16   IOSTANDARD LVCMOS33 } [get_ports { Leds[11] }];
+set_property -dict { PACKAGE_PIN V15   IOSTANDARD LVCMOS33 } [get_ports { Leds[12] }];
+set_property -dict { PACKAGE_PIN V14   IOSTANDARD LVCMOS33 } [get_ports { Leds[13] }];
+set_property -dict { PACKAGE_PIN V12   IOSTANDARD LVCMOS33 } [get_ports { Leds[14] }];
+set_property -dict { PACKAGE_PIN V11   IOSTANDARD LVCMOS33 } [get_ports { Leds[15] }];
+
 # Clock (100 MHz)
 set_property -dict { PACKAGE_PIN E3 IOSTANDARD LVCMOS33 } [get_ports { Clk100MHz }];
 create_clock -add -name sys_clk_pin -period 10.00 -waveform {0 5} [get_ports { Clk100MHz }];
