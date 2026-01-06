38ca431e7ec95416e0b1c7d27886646af6459e7a
EliasDeHondt <Elias.dehondt@outlook.com>
2026-01-06 21:21:18 +0100

    feat: Implement line detection function with Hough transform and visualization

--- DigitalImageProcessing/Session2/Example1.m
+++ DigitalImageProcessing/Session2/Example1.m
@@ -19,15 +19,3 @@ fprintf('Double pollen value: %f\n', pollen_d(x,y));
 
 fprintf('Original bone value: %d\n', bone(x,y));
 fprintf('Double bone value: %f\n', bone_d(x,y));
-
-pollen = imread('pollen.tif');
-bone   = imread('bone-scan-GE.tif');
-imtool(pollen,[]);
-imtool(bone,[]);
-pollen_d = im2double(pollen);
-bone_d   = im2double(bone);
-[x,y] = deal(100,150);
-fprintf('Original pollen value: %d\n', pollen(x,y));
-fprintf('Double pollen value: %f\n', pollen_d(x,y));
-fprintf('Original bone value: %d\n', bone(x,y));
-fprintf('Double bone value: %f\n', bone_d(x,y));
