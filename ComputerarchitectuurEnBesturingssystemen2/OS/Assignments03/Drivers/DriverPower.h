26638f2b3cdf852ea13897746542a6822df87c60
EliasDeHondt <Elias.dehondt@outlook.com>
2026-04-22 09:00:20 +0200

    Add initial implementation of Linebot project with CPU stats task and memory mapping
    - Created Atmel Studio solution and project files for LinebotTemplate1.
    - Added component info XML for project dependencies.
    - Implemented CPU stats task to monitor CPU load and provide statistics.
    - Developed hooks for FreeRTOS to handle memory allocation failures and stack overflows.
    - Configured hardware settings in hwconfig.h for USART and TWI drivers.
    - Implemented main application logic in main.c, initializing drivers and starting the FreeRTOS scheduler.
    - Added memory mapping functionality to display memory usage and task information.
    - Included various driver files for peripherals such as ADC, LED, and motor control.

---
+++ ComputerarchitectuurEnBesturingssystemen2/OS/Assignments03/Drivers/DriverPower.h
@@ -0,0 +1,10 @@
+/**
+    * @author EliasDH Team
+    * @see https://eliasdh.com
+    * @since 10/03/2026
+**/
+#include "hwconfig.h"
+
+void DriverPowerInit(void);
+
+void DriverPowerVccAuxSet(uint8_t State);
